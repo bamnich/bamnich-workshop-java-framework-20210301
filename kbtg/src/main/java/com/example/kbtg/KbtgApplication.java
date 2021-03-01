81cd3ad88197d8fc03c74d77a11debe0da5dfec9
K0966122 <nichcharee.s@kbtg.tech>
2021-03-01 14:50:50 +0700

    [Update] IoC and DI

--- kbtg/src/main/java/com/example/kbtg/KbtgApplication.java
+++ kbtg/src/main/java/com/example/kbtg/KbtgApplication.java
@@ -7,7 +7,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KbtgApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KbtgApplication.class, args);
+		// IoC Container => Context, Bean  => Spring
+		// IoC (Inversion of Control)
+		// DI (Dependency Injection)
+		ConfigurableApplicationContext context
+				= SpringApplication.run(KbtgApplication.class, args);
+		String[] beans = context.getBeanDefinitionNames();
+		for (String bean : beans) {
+			out.println(bean);
+		}
+		out.println(context.getBeanDefinitionCount());
+//		// Demo :: Singleton instance
+//		User user1 = context.getBean(User.class);
+//		user1.setName("Somkiat");
+//		out.println(user1.getName()); // 1
+//
+//		User user2 = context.getBean(User.class);
+//		user2.setName("User 2");
+//		out.println(user2.getName()); // 2
+//		out.println(user1.getName()); // 3
+
 	}
 
 }
