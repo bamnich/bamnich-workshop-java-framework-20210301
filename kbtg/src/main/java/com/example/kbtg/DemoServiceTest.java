8a36f71804bbe960332c32f782e9ce1806896191
K0966122 <nichcharee.s@kbtg.tech>
2021-03-01 15:05:42 +0700

    [Update] Use JUnit5

--- kbtg/src/main/java/com/example/kbtg/DemoServiceTest.java
+++ kbtg/src/main/java/com/example/kbtg/DemoServiceTest.java
@@ -24,23 +24,44 @@ public class DemoServiceTest {
         assertThrows(RuntimeException.class, () -> {
             DemoService demoService = new DemoService();
             demoService.setRandom(new Random6());
-            String actualResult = demoService.generateData("parunyu");
+            String actualResult = demoService.generateData("somkiat");
         });
 
     }
-}
 
+    @Test
+    public void throw_exception_1() {
+        DemoService demoService = new DemoService();
+        demoService.setRandom(new MockRandom(1));
+        try {
+            demoService.generateData("somkiat");
+            fail();
+        }catch (RuntimeException e) {
+            assertEquals("Invalid number with 1", e.getMessage());
+        }
+    }
 
-class Random5 extends Random {
-    @Override
-    public int nextInt(int bound) {
-        return 5;
+    @Test
+    public void throw_exception_with_juit5() {
+        DemoService demoService = new DemoService();
+        demoService.setRandom(new MockRandom(1));
+        // JUnit 5 style
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            demoService.generateData("somkiat");
+        });
+        assertEquals("Invalid number with 1", exception.getMessage());
     }
 }
 
-class Random6 extends Random {
+class MockRandom extends Random {
+    private final int result;
+
+    MockRandom(int result) {
+        this.result = result;
+    }
+
     @Override
     public int nextInt(int bound) {
-        return 6;
+        return result;
     }
 }
