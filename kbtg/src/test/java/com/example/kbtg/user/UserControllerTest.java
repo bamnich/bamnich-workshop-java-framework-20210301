c8fd48dd11c0a1ae33e73db8e5a5eeb688453e04
K0966122 <nichcharee.s@kbtg.tech>
2021-03-02 14:16:47 +0700

    [Update] Clean Code for Workshop 5

--- kbtg/src/test/java/com/example/kbtg/user/UserControllerTest.java
+++ kbtg/src/test/java/com/example/kbtg/user/UserControllerTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
