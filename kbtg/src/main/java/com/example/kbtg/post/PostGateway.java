b34c9a90ef7ce04f34fe533ee94e4218cdc0e282
K0966122 <nichcharee.s@kbtg.tech>
2021-03-02 17:02:14 +0700

    [Update] Working with Gateway

---
+++ kbtg/src/main/java/com/example/kbtg/post/PostGateway.java
@@ -0,0 +1,22 @@
+package com.example.kbtg.post;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class PostGateway {
+
+    private final RestTemplate restTemplate;
+    private final String postApiUrl;
+
+    @Autowired
+    public PostGateway(final RestTemplate restTemplate,
+                       @Value("${post.api.url}") final String postApiUrl) {
+        // Read from application.properties -> ${post.api.url}
+        this.restTemplate = restTemplate;
+        this.postApiUrl = postApiUrl;
+    }
+
+}
